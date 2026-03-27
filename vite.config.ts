ea211b17ed85c662c657875abfecf2e658df7cd9
30n1st <30n1st@gmail.com>
2026-03-27 01:47:08 +0000

    fix: define __APP_VERSION__ in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/pdf-export/',
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.gif', '**/*.ico', '**/*.webp'],
   build: {
     outDir: 'dist',
